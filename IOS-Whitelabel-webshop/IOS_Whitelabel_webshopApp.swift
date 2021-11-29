5292db7c3a24dd251bc6cb67f61e7790c10c2705
Semm6 <sem.dewilde6@gmail.com>
2021-11-29 23:48:00 +0100

    Initial Commit

---
+++ IOS-Whitelabel-webshop/IOS_Whitelabel_webshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Whitelabel_webshopApp.swift
+//  IOS-Whitelabel-webshop
+//
+//  Created by Sem de Wilde on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Whitelabel_webshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
